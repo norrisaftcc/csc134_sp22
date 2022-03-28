0bb67cd9f79a35a5c0d5d2ea9a8c894da1c6a849
Andrew Norris <norrisa@faytechcc.edu>
2022-03-28 11:05:54 -0400

    added while loop

--- SimpleMenu/main.cpp
+++ SimpleMenu/main.cpp
@@ -11,13 +11,27 @@ using namespace std;
 int main()
 {
     int userChoice = 0;
+    bool keepGoing = true;
+    while (keepGoing) {
 
-    cout << "Simple Menu" << endl;
-    cout << "1. Option 1" << endl;
-    cout << "2. Exit" << endl;
-    cout <<"Choose: ";
-    cin >> userChoice;
-    cout << "You picked " << userChoice << endl;
-
+        cout << "Simple Menu" << endl;
+        cout << "1. Option 1" << endl;
+        cout << "2. Exit" << endl;
+        cout <<"Choose: ";
+        cin >> userChoice;
+        //cout << "You picked " << userChoice << endl;
+        // Take action based on menu
+        if (userChoice == 1) {
+            cout << "You picked Option 1." << endl;
+        }
+        else if (userChoice == 2) {
+            cout << "Exiting ... goodbye." << endl;
+            keepGoing = false; // exit the loop
+        }
+        else {
+            // not any of the above options
+            cout << "Invalid input, please try again." << endl;
+        }
+    }
     return 0;
 }
