f3a35a57db5fda8492057835bc125a0374b69750
Andrew Norris <norrisa@faytechcc.edu>
2022-03-28 10:45:43 -0400

    First pass, just print menu

--- SimpleMenu/main.cpp
+++ SimpleMenu/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+// Print Menu
+// Get Input
+// If valid, do a thing
+// If not valid, repeat
+
+
 int main()
 {
+    int userChoice = 0;
+
     cout << "Simple Menu" << endl;
+    cout << "1. Option 1" << endl;
+    cout << "2. Exit" << endl;
+    cout <<"Choose: ";
+    cin >> userChoice;
+    cout << "You picked " << userChoice << endl;
+
     return 0;
 }
